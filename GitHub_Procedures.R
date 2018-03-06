e582be1812149dff8400263ab9cd491c75cc9c59
esiegfried <esiegfried@ucsb.edu>
2018-03-05 16:46:10 -0800

    How to guide for Git to RStudio

---
+++ GitHub_Procedures.R
@@ -0,0 +1,17 @@
+#"Push"/Send Updates to GIT Procedure: 
+# 1) Add code
+# 2) Save code
+# 3) Press commit
+# 4) Add comment in pop-up
+# 5) Click "Stage All" button
+# 6) Press commit button
+# 7) Press green arrow "Push"
+# 8) See pop-up confirming code sent to Git
+# 9) Go online and see the updated code
+
+#"Pulled"/Downloaded code from GIT Procedure:
+# In RStudio,
+# 1) Under Git tab, press blue down arrow
+# 2) Pop up will show file downloading
+# 3) File on computer will update automatically
+# 4) Begin adding to the code :)
