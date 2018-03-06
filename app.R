39a4828dfd668b50dfab9bde090530c2aa19ea82
esiegfried <esiegfried@ucsb.edu>
2018-03-05 16:37:39 -0800

    Added Download Procedure for RStudio

--- app.R
+++ app.R
@@ -1,6 +1,6 @@
 library(shinydashboard)
 
-#GIT Procedure: 
+#"Push"/Send Updates to GIT Procedure: 
 # 1) Add code
 # 2) Save code
 # 3) Press commit
@@ -11,7 +11,12 @@ library(shinydashboard)
 # 8) See pop-up confirming code sent to Git
 # 9) Go online and see the updated code
 
-#Changed this on GitHub
+#"Pulled"/Downloaded code from GIT Procedure:
+# In RStudio,
+# 1) Under Git tab, press blue down arrow
+# 2) Pop up will show file downloading
+# 3) File on computer will update automatically
+# 4) Begin adding to the code :)
 
 ui <- dashboardPage(
   dashboardHeader(title = "An App of Happiness"),
