b7bd05ec8984dc8f143385435a1a186b78e6d255
lcpowers <lcpowers@ucsb.edu>
2018-03-06 23:55:42 -0800

    No more funny business

--- app.R
+++ app.R
@@ -1,10 +1,5 @@
 library(shinydashboard)
-library(wesanderson) #2nd best to unicorns
-
-#Hey What's Up
-#Hello?
-#Knock,Knock
-#Who's there?
+library(wesanderson) 
 
 ui <- dashboardPage(skin = "purple",
   dashboardHeader(title = "The App of Happiness"),
